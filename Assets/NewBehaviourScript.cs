56341a9e0fc25a96b70ce1ce9e329dcc59810ae0
rStar <starlove1247@gmail.com>
2025-03-05 12:02:28 +0800

    練習 觸發跟碰撞

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log($"OnTriggerEnter2D");
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log($"OnCollisionEnter2D");
+    }
+}
