c1119d73bd5c59eb91d820ae8d4d53b75ee12f7e
yitung123 <tina2003920@gmail.com>
2025-03-12 11:50:20 +0800

    偵測跟碰撞的練習

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,11 +5,15 @@ public class NewBehaviourScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log($"OnTriggerEnter2D");
+       // col.gameObject.SetActive(false);
+       GameObject qrCode = GameObject.Find("QRCode");
+       qrCode.SetActive(false);
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
         Debug.Log($"OnCollisionEnter2D");
+        col.gameObject.SetActive(false);
     }
 
 }
